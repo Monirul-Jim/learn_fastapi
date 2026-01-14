b44b2ed0e4cf61514ec712afbecaaedabdd7b4c1
Monirul Jim <monirulislamjim6640@gmail.com>
2026-01-14 19:44:35 +0600

    config env

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
 from pydantic import BaseModel,HttpUrl
-app=FastAPI()
+import psycopg2
+from pydantic_settings import BaseSettings,SettingsConfigDict
+from psycopg2.extras import RealDictCursor
+import time
 
+class Settings(BaseSettings):
+    db_host:str
+    db_name:str
+    db_user:str
+    db_password:str
+    model_config=SettingsConfigDict(env_file=".env")
+settings=Settings()
+while True:
+    try:
+        conn=psycopg2.connect(host=settings.db_host,database=settings.db_name,user=settings.db_user,password=settings.db_password,ursor_factory=RealDictCursor)
+        cursor=conn.cursor()
+        print("Connection successfully")
+        break;
+    except Exception as error:
+        print("Datebase connection Failed")
+        print("Error",error)
+        time.sleep(2)
+
+
+app=FastAPI()
 #define request body schema
 class Course(BaseModel):
     name:str
